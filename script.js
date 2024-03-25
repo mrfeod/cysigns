260b53d09c9f9c7feb2f7f4a950b53b8bf3cdd12
Anton Sosnin <antsosnin@yandex.ru>
2024-03-25 19:16:10 +0200

    Add self-test mode

--- script.js
+++ script.js
@@ -2,43 +2,73 @@ let currentQuestion = 0;
 let correctCount = 0;
 let incorrectCount = 0;
 
+const Mode = {
+  LEARN: 'LEARN',
+  TEST: 'TEST',
+};
+
 function getRandomAnswers() {
   let answers = [...signs];
   answers.sort(() => Math.random() - 0.5);
   return answers.slice(0, 4);
 }
 
-function showQuestion() {
-  let question = getRandomQuestion();
-  document.getElementById('signImage').src = question.image;
+let showAnswer = function() {}
+
+function hideAnswer() {
   let answerText = document.getElementById('answer');
   answerText.innerText = '';
-  let answers = getRandomAnswers();
-  if (!answers.find(answer => answer.name === question.name)) {
-    answers[0] = question;
-    answers.sort(() => Math.random() - 0.5);
-  }
+}
+
+function showQuestion() {
+  document.getElementById('question').innerText = currentQuestion + 1;
+  document.getElementById('total').innerText = signs.length;
+
+  let question = getRandomQuestion();
+  document.getElementById('signImage').src = question.image;
+  hideAnswer();
+
+  let modeVal = document.getElementById('mode').checked;
+  let mode = modeVal ? Mode.TEST : Mode.LEARN;
+
   let answersDiv = document.getElementById('answers');
   answersDiv.innerHTML = '';
-  for (let i = 0; i < answers.length; i++) {
-    let answer = document.createElement('button');
-    answer.className = 'answer';
-    answer.innerText = answers[i].name;
-    answer.onclick = function() {
-      let toNext = false;
-      if (this.innerText === question.name) {
-        toNext = true;
-        correctCount++;
-      } else {
-        incorrectCount++;
-        let answerText = document.getElementById('answer');
-        answerText.innerText = `${question.name}`;
-      }
-      document.getElementById('correct').innerText = correctCount;
-      document.getElementById('incorrect').innerText = incorrectCount;
-      if (toNext) nextQuestion();
-    };
-    answersDiv.appendChild(answer);
+
+  showAnswer = function() {
+    let answerText = document.getElementById('answer');
+    answerText.innerText = `${question.name}`;
+  };
+
+  if (mode === Mode.TEST) {
+    document.getElementById('answers').style.display = 'none';
+    document.getElementById('showAnswerButton').style.display = 'block';
+  } else {
+    document.getElementById('showAnswerButton').style.display = 'none';
+    document.getElementById('answers').style.display = 'grid';
+    let answers = getRandomAnswers();
+    if (!answers.find(answer => answer.name === question.name)) {
+      answers[0] = question;
+      answers.sort(() => Math.random() - 0.5);
+    }
+    for (let i = 0; i < answers.length; i++) {
+      let answer = document.createElement('button');
+      answer.className = 'answer';
+      answer.innerText = answers[i].name;
+      answer.onclick = function() {
+        let toNext = false;
+        if (this.innerText === question.name) {
+          toNext = true;
+          correctCount++;
+        } else {
+          incorrectCount++;
+          showAnswer();
+        }
+        document.getElementById('correct').innerText = correctCount;
+        document.getElementById('incorrect').innerText = incorrectCount;
+        if (toNext) nextQuestion();
+      };
+      answersDiv.appendChild(answer);
+    }
   }
 }
 
@@ -62,6 +92,11 @@ function nextQuestion() {
   } else {
     alert('Test finished!');
   }
+}
+
+function switchMode() {
+  currentQuestion = 0;
+  showQuestion();
 }
 
 showQuestion();

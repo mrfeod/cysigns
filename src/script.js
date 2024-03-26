23cee8adbdde3af3c8c535be55565d9ce51a7ec7
Anton Sosnin <antsosnin@yandex.ru>
2024-03-26 21:31:42 +0200

    Beautiful buttons on answer

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 let currentQuestion = 0;
 let correctCount = 0;
 let incorrectCount = 0;
-
-let answersDisplay = getElementDisplay('answers');
-let showAnswerButtonDisplay = getElementDisplay('showAnswerButton');
+const buttonsCount = 4;
+const answersDisplay = getElementDisplay('answers');
+const showAnswerButtonDisplay = getElementDisplay('showAnswerButton');
 
 const Mode = {
   LEARN: 'LEARN',
@@ -22,10 +22,10 @@ function setElementText(elementId, text) {
   return document.getElementById(elementId).innerText = text;
 }
 
-function getRandomAnswers() {
+function getRandomAnswers(count) {
   let answers = [...signs];
   answers.sort(() => Math.random() - 0.5);
-  return answers.slice(0, 4);
+  return answers.slice(0, count);
 }
 
 let showAnswer = function() {}
@@ -55,20 +55,26 @@ function showQuestion() {
   setElementText('total', signs.length);
   setElementText('correct', correctCount);
   setElementText('incorrect', incorrectCount);
+  hideAnswer();
 
-  let question = getRandomQuestion();
+  for (let i = 0; i < buttonsCount; i++) {
+    let answer = document.getElementById('answerButton' + (i + 1));
+    answer.classList.remove('btn-success');
+    answer.classList.remove('btn-error');
+    answer.classList.remove('disabled');
+  }
+
+  const question = getRandomQuestion();
   let image = document.getElementById('signImage');
   image.src = question.image;
   image.alt = question.name;
-  hideAnswer();
-
-  let modeVal = document.getElementById('mode').checked;
-  let mode = modeVal ? Mode.TEST : Mode.LEARN;
 
   showAnswer = function() {
     let answerText = document.getElementById('answerText');
     answerText.innerText = `${question.name}`;
   };
+
+  const mode = document.getElementById('mode').checked ? Mode.TEST : Mode.LEARN;
 
   if (mode === Mode.TEST) {
     setElementDisplay('answers', 'none');
@@ -79,24 +85,32 @@ function showQuestion() {
     setElementDisplay('showAnswerButton', 'none');
     setElementDisplay('answers', answersDisplay);
 
-    let answers = getRandomAnswers();
+    let answers = getRandomAnswers(buttonsCount);
     if (!answers.find(answer => answer.name === question.name)) {
       answers[0] = question;
       answers.sort(() => Math.random() - 0.5);
     }
-    for (let i = 0; i < answers.length; i++) {
-      let answer = document.getElementById('answerButton' + (i + 1));
-      answer.innerText = answers[i].name;
-      answer.onclick = function() {
-        let toNext = false;
-        if (this.innerText === question.name) {
-          toNext = true;
+
+    let answerIndex = answers.findIndex(a => a.name === question.name) + 1;
+    let theAnswerButton = document.getElementById('answerButton' + answerIndex);
+
+    for (let i = 0; i < buttonsCount; i++) {
+      const id = 'answerButton' + (i + 1);
+      let button = document.getElementById(id);
+      button.innerText = answers[i].name;
+      button.onclick = function() {
+        for (let i = 0; i < buttonsCount; i++) {
+          const id = 'answerButton' + (i + 1);
+          document.getElementById(id).classList.add('disabled');
+        }
+        theAnswerButton.classList.add('btn-success');
+        if (button === theAnswerButton) {
           correctCount++;
+          setTimeout(nextQuestion, 150);
         } else {
           incorrectCount++;
-          showAnswer();
+          button.classList.add('btn-error');
         }
-        if (toNext) nextQuestion();
       };
     }
   }
